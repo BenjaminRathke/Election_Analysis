2269a33033c55d44acd989221828c3239703b62f
Ben Rathke <ben.rathke@gmail.com>
2021-06-24 11:29:04 -0600

    Removing .gitkeep placeholder files, adding the analysis .txt file, and updating the PyPoll.py code file.

--- PyPoll.py
+++ PyPoll.py
@@ -5,20 +5,24 @@
 # 4. The total number of votes each candidate won
 # 5. The winner of the election based on the popular vote.
 
+#Add our dependencies.
 import csv
 import os
 
 # Assign a variable for the file to load and the path.
 file_to_load = os.path.join("Resources", "election_results.csv")
-
-# Open the election results and read the file.
-with open(file_to_load) as election_data:
-    
-    # Print the file object
-    print(election_data)
-    
+  
 # Create a filename variable to a direct or indirect path to the file.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
-#Using the open() fuction with the "w" mode we will write data to the file.
-open(file_to_save, "w")
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+    # Read the file object with the reader function
+    file_reader = csv.reader(election_data)
+    
+    # Print the header row.
+    headers = next(file_reader)
+    print(headers)
+    
+
+        
