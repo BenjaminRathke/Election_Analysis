99d0bd39c166b922cfa7cbb6c28ae9945dcd101f
Ben Rathke <ben.rathke@gmail.com>
2021-06-28 09:35:03 -0600

    Updating PyPoll_Challenge.py and election_analysis.txt for final deliverable.

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -98,7 +98,7 @@ with open(file_to_save, "w") as txt_file:
         # 6c: Calculate the percentage of votes for the county.
         ballots_percentage = float(ballots) / float(total_votes) * 100
         county_results = (
-                f"{county_name}: {ballots_percentage:.1f}% ({ballots:,})\n")
+            f"{county_name}: {ballots_percentage:.1f}% ({ballots:,})\n")
         # 6d: Print the county results to the terminal.
         print(county_results)
         # 6e: Save the county votes to a text file.
@@ -111,10 +111,14 @@ with open(file_to_save, "w") as txt_file:
              largest_percentage = ballots_percentage            
 
     # 7: Print the county with the largest turnout to the terminal.
-
+    winning_county_summary = (
+            f"\n-------------------------\n"
+            f"Largest County Turnout: {largest_county}\n"
+            f"-------------------------\n")
+    print(winning_county_summary)
 
     # 8: Save the county with the largest turnout to a text file.
-
+    txt_file.write(winning_county_summary)
 
     # Save the final candidate vote count to the text file.
     for candidate_name in candidate_votes:
